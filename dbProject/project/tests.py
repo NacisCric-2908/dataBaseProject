2b3b239a9111747cf10852a935fa99f8225ef8e4
NacisCric-2908 <juandiegoc2908@gmail.com>
2025-11-25 12:01:36 -0500

    FRONTEND Y TEST, INSTRUCCIONS ACTUALIZADO

--- dbProject/project/tests.py
+++ dbProject/project/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomeViewTests(TestCase):
+	def setUp(self):
+		self.client = Client()
+
+	def test_home_status_code(self):
+		resp = self.client.get(reverse('home'))
+		self.assertEqual(resp.status_code, 200)
+
+	def test_home_contains_form_fields(self):
+		resp = self.client.get(reverse('home'))
+		self.assertContains(resp, 'name="txtCodigo"')
+		self.assertContains(resp, 'name="txtNombre"')
+		self.assertContains(resp, 'name="txtApellido"')
+		self.assertContains(resp, 'name="txtNumDocumento"')
+		self.assertContains(resp, 'name="TipoDocumento"')
